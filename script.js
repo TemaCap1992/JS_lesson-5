4389ad9855d5fa876667d14bb12c9356dc06e20d
Artem <62652008+TemaCap1992@users.noreply.github.com>
2020-08-07 18:53:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,130 @@
+"use strict"
+
+// Task 1
+// ===================================
+// let user {
+
+// }
+
+// object.generator()
+
+
+// propertyTitle
+// propertyValue
+
+// function User(userName, userAge) {
+//     this.name = userName;
+//     this.age = userAge;
+// }
+
+// let userAdmin = new User('Vasya', 42);
+// console.log(userAdmin);
+
+
+
+
+// console.log(zhiguly);
+// console.log(mercedes);
+
+// let user = {
+//     name: 'Kolya',
+//     surname: 'Vasiliev',
+
+// };
+
+// let title;
+// let value;
+
+// let createUser : function(title, value) {
+//     set (title) {
+//         this.title = title
+//     }
+//     set (value)
+// };
+
+// createUser();
+
+
+
+
+
+
+// set(value) {
+//     this.name = value.split(' ')[0];
+//     this.surname = value.split(' ')[1];
+// }
+
+
+// console.log(user.fullName);
+
+// user.fullName = 'Sveta Ivanova';
+
+// console.log(user);
+
+
+// let user = {
+//     name: "John",
+//     surname: "Smith",  
+
+// };
+
+// this.title = title;
+// set this.title(value){
+//     this.value = value
+
+// } 
+
+// user.newProperty = "AliceCooper";
+
+
+
+
+
+//   // set fullName запустится с данным значением
+
+// console.log(user);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let divByTag = document.getElementsByTagName('div');
+console.log(divByTag);
+
+let divByClass = document.getElementsByClassName('box')
+divByClass[0].style.background = 'red';
+console.log(divByClass);
+
+let divById = document.getElementById('fox')
+divById;
+console.log(divById);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
